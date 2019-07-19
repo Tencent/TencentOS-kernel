86055c32bbbd5f700ad4c91ecf681ddd6d48ed3e
Xiaoming Gao <newtongao@tencent.com>
2019-07-19 15:12:33 +0800

    kernel/panic: set crash_kexec_post_notifiers default to true
    Signed-off-by: Xiaoming Gao <newtongao@tencent.com>

--- kernel/panic.c
+++ kernel/panic.c
@@ -37,7 +37,7 @@ static unsigned long tainted_mask;
 static int pause_on_oops;
 static int pause_on_oops_flag;
 static DEFINE_SPINLOCK(pause_on_oops_lock);
-bool crash_kexec_post_notifiers;
+bool crash_kexec_post_notifiers = true;
 int panic_on_warn __read_mostly;
 
 int panic_timeout = CONFIG_PANIC_TIMEOUT;
