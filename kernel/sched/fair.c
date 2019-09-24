0e74e9c6dd1cb47368925fbc02bedb08cb82a29b
Zhiguang Peng <zgpeng@tencent.com>
2019-09-24 14:34:49 +0800

    sched:fair: fix crash in list_add_leaf_cfs_rq due to bad tmp_alone_branch
    Signed-off-by: Zhiguang Peng <zgpeng@tencent.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -364,6 +364,11 @@ static inline void list_add_leaf_cfs_rq(struct cfs_rq *cfs_rq)
 static inline void list_del_leaf_cfs_rq(struct cfs_rq *cfs_rq)
 {
 	if (cfs_rq->on_list) {
+		struct rq *rq = rq_of(cfs_rq);
+
+		if (rq->tmp_alone_branch == &cfs_rq->leaf_cfs_rq_list)
+			rq->tmp_alone_branch = cfs_rq->leaf_cfs_rq_list.prev;
+
 		list_del_rcu(&cfs_rq->leaf_cfs_rq_list);
 		cfs_rq->on_list = 0;
 	}
