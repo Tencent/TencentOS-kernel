f540f245de4d7c19cf014c513dac60b99e472571
Xiaoming Gao <newtongao@tencent.com>
2020-01-21 12:08:58 +0800

    vm: fix isolate max_map_count, inherit the value of the parent namespace instead
    Signed-off-by: Xiaoming Gao <newtongao@tencent.com>

--- kernel/pid_namespace.c
+++ kernel/pid_namespace.c
@@ -137,7 +137,7 @@ static struct pid_namespace *create_pid_namespace(struct user_namespace *user_ns
 	ns->user_ns = get_user_ns(user_ns);
 	ns->ucounts = ucounts;
 	ns->nr_hashed = PIDNS_HASH_ADDING;
-	ns->max_map_count = DEFAULT_MAX_MAP_COUNT;
+	ns->max_map_count = parent_pid_ns->max_map_count;
 	INIT_WORK(&ns->proc_work, proc_cleanup_work);
 
 	set_bit(0, ns->pidmap[0].page);
