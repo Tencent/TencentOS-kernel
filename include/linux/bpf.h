6aab040e759f145532a6049e599f00913f8196f0
Alexei Starovoitov <ast@kernel.org>
2020-07-01 18:10:18 -0700

    bpf: Add bpf_prog iterator
    [upstream commit a228a64fc1e4428e2b96dc68e9ad3c447095c9e7]
    
    It's mostly a copy paste of commit 6086d29def80 ("bpf: Add bpf_map iterator")
    that is use to implement bpf_seq_file opreations to traverse all bpf programs.
    
    v1->v2: Tweak to use build time btf_id
    
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Yonghong Song <yhs@fb.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -1031,6 +1031,7 @@ void *bpf_map_area_mmapable_alloc(u64 size, int numa_node);
 void bpf_map_area_free(void *base);
 void bpf_map_init_from_attr(struct bpf_map *map, union bpf_attr *attr);
 struct bpf_map *bpf_map_get_curr_or_next(u32 *id);
+struct bpf_prog *bpf_prog_get_curr_or_next(u32 *id);
 
 extern int sysctl_unprivileged_bpf_disabled;
 
