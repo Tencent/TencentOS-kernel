0a131efec87aa74a94d92f8d0562366e144f8331
Yonghong Song <yhs@fb.com>
2020-07-22 12:51:56 -0700

    bpf: Fix pos computation for bpf_iter seq_ops->start()
    [upstream commit 3f9969f2c040ba2ba635b6b5a7051f404bcc634d]
    
    Currently, the pos pointer in bpf iterator map/task/task_file
    seq_ops->start() is always incremented.
    This is incorrect. It should be increased only if
    *pos is 0 (for SEQ_START_TOKEN) since these start()
    function actually returns the first real object.
    If *pos is not 0, it merely found the object
    based on the state in seq->private, and not really
    advancing the *pos. This patch fixed this issue
    by only incrementing *pos if it is 0.
    
    Note that the old *pos calculation, although not
    correct, does not affect correctness of bpf_iter
    as bpf_iter seq_file->read() does not support llseek.
    
    This patch also renamed "mid" in bpf_map iterator
    seq_file private data to "map_id" for better clarity.
    
    Fixes: 6086d29def80 ("bpf: Add bpf_map iterator")
    Fixes: eaaacd23910f ("bpf: Add task and task/file iterator targets")
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200722195156.4029817-1-yhs@fb.com

--- kernel/bpf/task_iter.c
+++ kernel/bpf/task_iter.c
@@ -45,7 +45,8 @@ static void *task_seq_start(struct seq_file *seq, loff_t *pos)
 	if (!task)
 		return NULL;
 
-	++*pos;
+	if (*pos == 0)
+		++*pos;
 	return task;
 }
 
@@ -204,7 +205,8 @@ static void *task_file_seq_start(struct seq_file *seq, loff_t *pos)
 		return NULL;
 	}
 
-	++*pos;
+	if (*pos == 0)
+		++*pos;
 	info->task = task;
 	info->files = files;
 
