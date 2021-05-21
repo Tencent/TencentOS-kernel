d253516e8716930869c037539fb9b43b25ca26a8
Kaixu Xia <kaixuxia@tencent.com>
2021-05-21 10:00:22 +0800

    Revert "modules: mark ref_module static"
    This reverts commit af16ca3bc733467eb84ea1350292ea7cce84a6f0.

--- kernel/module.c
+++ kernel/module.c
@@ -867,7 +867,7 @@ static int add_module_usage(struct module *a, struct module *b)
 }
 
 /* Module a uses b: caller needs module_mutex() */
-static int ref_module(struct module *a, struct module *b)
+int ref_module(struct module *a, struct module *b)
 {
 	int err;
 
@@ -886,6 +886,7 @@ static int ref_module(struct module *a, struct module *b)
 	}
 	return 0;
 }
+EXPORT_SYMBOL_GPL(ref_module);
 
 /* Clear the unload stuff of the module. */
 static void module_unload_free(struct module *mod)
@@ -1166,10 +1167,11 @@ static inline void module_unload_free(struct module *mod)
 {
 }
 
-static int ref_module(struct module *a, struct module *b)
+int ref_module(struct module *a, struct module *b)
 {
 	return strong_try_module_get(b);
 }
+EXPORT_SYMBOL_GPL(ref_module);
 
 static inline int module_unload_init(struct module *mod)
 {
