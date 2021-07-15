68fdd6d05d8b1c3ab3673b63af4ddc88bd85822b
Alexei Starovoitov <ast@kernel.org>
2021-07-14 17:54:11 -0700

    bpf: Prevent pointer mismatch in bpf_timer_init.
    [upstream commit 3e8ce29850f1839d0603f925b30be9d8a4329917]
    
    bpf_timer_init() arguments are:
    1. pointer to a timer (which is embedded in map element).
    2. pointer to a map.
    Make sure that pointer to a timer actually belongs to that map.
    
    Use map_uid (which is unique id of inner map) to reject:
    inner_map1 = bpf_map_lookup_elem(outer_map, key1)
    inner_map2 = bpf_map_lookup_elem(outer_map, key2)
    if (inner_map1 && inner_map2) {
        timer = bpf_map_lookup_elem(inner_map1);
        if (timer)
            // mismatch would have been allowed
            bpf_timer_init(timer, inner_map2);
    }
    
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Link: https://lore.kernel.org/bpf/20210715005417.78572-6-alexei.starovoitov@gmail.com

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -50,7 +50,14 @@ struct bpf_reg_state {
 		/* valid when type == CONST_PTR_TO_MAP | PTR_TO_MAP_VALUE |
 		 *   PTR_TO_MAP_VALUE_OR_NULL
 		 */
-		struct bpf_map *map_ptr;
+		struct {
+			struct bpf_map *map_ptr;
+			/* To distinguish map lookups from outer map
+			 * the map_uid is non-zero for registers
+			 * pointing to inner maps.
+			 */
+			u32 map_uid;
+		};
 
 		u32 btf_id; /* for PTR_TO_BTF_ID */
 		u32 mem_size; /* for PTR_TO_MEM | PTR_TO_MEM_OR_NULL */
