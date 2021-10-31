db350574277356b918bd2ea1b0b67529609d0a07
caelli <caelli@tencent.com>
2021-10-31 17:25:57 +0800

    cgroupfs: refactor cgroup resource statistics for reuse.
    Signed-off-by: caelli <caelli@tencent.com>
    Reviewed-by: Peng Hao <flyingpeng@tencent.com>
    Reviewed-by: Bin Lai <robinlai@tencent.com>

--- kernel/sched/cpuacct.c
+++ kernel/sched/cpuacct.c
@@ -301,9 +301,8 @@ static int cpuacct_stats_show(struct seq_file *sf, void *v)
 	return 0;
 }
 
-static int cpuacct_uptime_show(struct seq_file *sf, void *v)
+static int cpuacct_uptime_show_comm(struct seq_file *sf, void *v, struct cpuacct *ca)
 {
-	struct cpuacct *ca = css_ca(seq_css(sf));
 	struct timespec64 uptime, idle, curr;
 	u64 idletime = 0;
 	u32 cpu, rem;
@@ -336,6 +335,12 @@ static int cpuacct_sli_max_show(struct seq_file *sf, void *v)
 	struct cgroup *cgrp = seq_css(sf)->cgroup;
 
 	return sli_schedlat_max_show(sf, cgrp);
+}
+
+static int cpuacct_uptime_show(struct seq_file *sf, void *v)
+{
+	struct cpuacct *ca = css_ca(seq_css(sf));
+	return cpuacct_uptime_show_comm(sf, v, ca);
 }
 
 static struct cftype files[] = {
