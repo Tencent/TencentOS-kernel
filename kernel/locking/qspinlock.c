042d164b2bd369f82b3bee00069a6224a000232f
Mungerjiang <mungerjiang@tencent.com>
2021-11-04 13:02:26 +0800

    locking/qspinlock: Rename mcs lock/unlock macros and make them more generic
    	The mcs unlock macro (arch_mcs_lock_handoff) should accept the value to be
    	stored into the lock argument as another argument. This allows using the
    	same macro in cases where the value to be stored when passing the lock is
    	different from 1.
    
    	Signed-off-by: Alex Kogan <alex.kogan@oracle.com>
    	Reviewed-by: Steve Sistare <steven.sistare@oracle.com>
    	Reviewed-by: Waiman Long <longman@redhat.com>

--- kernel/locking/qspinlock.c
+++ kernel/locking/qspinlock.c
@@ -470,7 +470,7 @@ pv_queue:
 		WRITE_ONCE(prev->next, node);
 
 		pv_wait_node(node, prev);
-		arch_mcs_spin_lock_contended(&node->locked);
+		arch_mcs_spin_wait(&node->locked);
 
 		/*
 		 * While waiting for the MCS lock, the next pointer may have
@@ -549,7 +549,7 @@ locked:
 	if (!next)
 		next = smp_cond_load_relaxed(&node->next, (VAL));
 
-	arch_mcs_spin_unlock_contended(&next->locked);
+	arch_mcs_lock_handoff(&next->locked, 1);
 	pv_kick_node(lock, next);
 
 release:
