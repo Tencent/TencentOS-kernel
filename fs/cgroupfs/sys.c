1fde852cfeb9a930a8e1145102a711f3592c8b66
caelli <caelli@tencent.com>
2021-12-20 15:19:49 +0800

    cgroupfs: add files inside sys
    Add files inside /cgroupfs/sys/devices/system/cpu/, currently sysfs
    mounted on /sys/ before cgroupfs is required, we use d_automount
    interface to switch from cgroupfs to sysfs.
    
    Signed-off-by: caelli <caelli@tencent.com>
    Reviewed-by: Bin Lai <robinlai@tencent.com>

--- fs/cgroupfs/sys.c
+++ fs/cgroupfs/sys.c
@@ -1,13 +1,22 @@
 #include <linux/fs.h>
 #include <linux/fs_context.h>
+#include <linux/init_task.h>
+#include <linux/fs_struct.h>
+#include "../internal.h"
 #include "cgroupfs.h"
 
 extern int cpu_get_max_cpus(struct task_struct *p);
 extern int cpuset_cgroups_cpu_allowed(struct task_struct *task, int cpu, int once);
 
-static int cgroupfs_dop_revalidate(struct dentry *dentry, unsigned int flags)
+static int __attribute__((unused)) cgroupfs_dop_revalidate(
+			struct dentry *dentry, unsigned int flags)
 {
-	return 0;
+	cgroupfs_entry_t *en;
+
+	en = dentry->d_inode->i_private;
+	if (en->cgroupfs_type & CGROUPFS_TYPE_CPUDIR)
+		return 0;
+	return 1;
 }
 
 int cgroupfs_cpu_dir_filter(int cpu, int *max_cpu, int *counted_cpu, int once)
@@ -24,9 +33,26 @@ int cgroupfs_cpu_dir_filter(int cpu, int *max_cpu, int *counted_cpu, int once)
 	return 0;
 }
 
+static struct vfsmount *cgroupfs_dop_auto_mount(struct path *target)
+{
+	cgroupfs_entry_t *en;
+	struct path path;
+	int err;
+
+	en = target->dentry->d_inode->i_private;
+	err = vfs_path_lookup(init_task.fs->root.dentry, init_task.fs->root.mnt,
+		en->private, 0, &path);
+	if (err)
+		return ERR_PTR(err);
+	dput(target->dentry);
+	target->dentry = path.dentry;
+	target->mnt = path.mnt;
+	return 0;
+}
+
 const struct dentry_operations cgroupfs_dir_dops = {
-	.d_revalidate   = cgroupfs_dop_revalidate,
 	.d_delete       = always_delete_dentry,
+	.d_automount    = cgroupfs_dop_auto_mount,
 };
 
 void cgroupfs_set_sys_dops(cgroupfs_entry_t *en)
