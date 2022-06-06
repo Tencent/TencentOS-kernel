56538e41ad7cb9de9ecbd0458dd50f4f5c4d8e42
Kairui Song <kasong@tencent.com>
2022-06-06 13:49:03 +0800

    x86: kdump: use macro CRASH_ADDR_LOW_MAX in functions reserve_crashkernel()
    From: Chen Zhou <chenzhou10@huawei.com>
    Link: https://lkml.org/lkml/2021/1/30/53
    Link: https://github.com/openeuler-mirror/kernel/commit/a2e0b4351d5481fcfda10957a95c078b38732e1d
    
    To make the functions reserve_crashkernel() as generic,
    replace some hard-coded numbers with macro CRASH_ADDR_LOW_MAX.
    
    Signed-off-by: Chen Zhou <chenzhou10@huawei.com>
    Tested-by: John Donnelly <John.p.donnelly@oracle.com>
    Signed-off-by: Chen Zhou <chenzhou10@huawei.com>
    Acked-by: Xie XiuQi <xiexiuqi@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: Kairui Song <kasong@tencent.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -551,8 +551,9 @@ static void __init reserve_crashkernel(void)
 	if (!crash_base) {
 		/*
 		 * Set CRASH_ADDR_LOW_MAX upper bound for crash memory,
-		 * crashkernel=x,high reserves memory over 4G, also allocates
-		 * 256M extra low memory for DMA buffers and swiotlb.
+		 * crashkernel=x,high reserves memory over CRASH_ADDR_LOW_MAX,
+		 * also allocates 256M extra low memory for DMA buffers
+		 * and swiotlb.
 		 * But the extra memory is not required for all machines.
 		 * So try low memory first and fall back to high memory
 		 * unless "crashkernel=size[KMG],high" is specified.
@@ -586,7 +587,7 @@ static void __init reserve_crashkernel(void)
 		return;
 	}
 
-	if (crash_base >= (1ULL << 32) && reserve_crashkernel_low()) {
+	if (crash_base >= CRASH_ADDR_LOW_MAX && reserve_crashkernel_low()) {
 		memblock_free(crash_base, crash_size);
 		return;
 	}
