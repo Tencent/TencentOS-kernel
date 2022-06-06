342acc089296f6cbd7a5dcc7000b2d4c2aa4c9e3
Kairui Song <kasong@tencent.com>
2022-06-06 13:48:56 +0800

    x86: kdump: make the lower bound of crash kernel reservation consistent
    From: Chen Zhou <chenzhou10@huawei.com>
    Link: https://lkml.org/lkml/2021/1/30/53
    Link: https://github.com/openeuler-mirror/kernel/commit/8882ba540e3c6467671a593779018b269fcfb036
    
    The lower bounds of crash kernel reservation and crash kernel low
    reservation are different, use the consistent value CRASH_ALIGN.
    
    Suggested-by: Dave Young <dyoung@redhat.com>
    Signed-off-by: Chen Zhou <chenzhou10@huawei.com>
    Tested-by: John Donnelly <John.p.donnelly@oracle.com>
    Signed-off-by: Chen Zhou <chenzhou10@huawei.com>
    Acked-by: Xie XiuQi <xiexiuqi@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: Kairui Song <kasong@tencent.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -498,7 +498,7 @@ static int __init reserve_crashkernel_low(void)
 			return 0;
 	}
 
-	low_base = memblock_find_in_range(0, 1ULL << 32, low_size, CRASH_ALIGN);
+	low_base = memblock_find_in_range(CRASH_ALIGN, CRASH_ADDR_LOW_MAX, low_size, CRASH_ALIGN);
 	if (!low_base) {
 		pr_err("Cannot reserve %ldMB crashkernel low memory, please try smaller size.\n",
 		       (unsigned long)(low_size >> 20));
