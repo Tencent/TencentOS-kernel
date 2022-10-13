42055d7dcade7005c411f3f153e7b54155743a5d
liuchun <kaicliu@tencent.com>
2022-10-13 11:34:36 +0800

    drm/hisilicon: Support i2c driver algorithms for bit-shift adapters
    Adding driver implementation to support i2c driver algorithms for
    bit-shift adapters, so hibmc will using the interface provided by
    drm to read edid.
    
    Signed-off-by: Tian Tao <tiantao6@hisilicon.com>
    Reviewed-by: Thomas Zimmermann <tzimmermann@suse.de>

--- drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_drv.h
+++ drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_drv.h
@@ -14,6 +14,12 @@
 #ifndef HIBMC_DRM_DRV_H
 #define HIBMC_DRM_DRV_H
 
+#include <linux/gpio/consumer.h>
+#include <linux/i2c-algo-bit.h>
+#include <linux/i2c.h>
+#include <drm/drm_encoder.h>
+
+#include <drm/drm_edid.h>
 #include <drm/drm_fb_helper.h>
 #include <drm/drm_framebuffer.h>
 
@@ -31,6 +37,13 @@ struct hibmc_fbdev {
 	int size;
 };
 
+struct hibmc_connector {
+	struct drm_connector base;
+
+	struct i2c_adapter adapter;
+	struct i2c_algo_bit_data bit_data;
+};
+
 struct hibmc_drm_private {
 	/* hw */
 	void __iomem   *mmio;
@@ -41,6 +54,8 @@ struct hibmc_drm_private {
 
 	/* drm */
 	struct drm_device  *dev;
+ 	struct drm_encoder encoder;
+	struct hibmc_connector connector;
 	bool mode_config_initialized;
 
 	/* fbdev */
@@ -48,6 +63,16 @@ struct hibmc_drm_private {
 };
 
 #define to_hibmc_framebuffer(x) container_of(x, struct hibmc_framebuffer, fb)
+
+static inline struct hibmc_connector *to_hibmc_connector(struct drm_connector *connector)
+{
+	return container_of(connector, struct hibmc_connector, base);
+}
+
+static inline struct hibmc_drm_private *to_hibmc_drm_private(struct drm_device *dev)
+{
+	return dev->dev_private;
+}
 
 void hibmc_set_power_mode(struct hibmc_drm_private *priv,
 			  unsigned int power_mode);
@@ -70,6 +95,7 @@ int hibmc_mm_init(struct hibmc_drm_private *hibmc);
 void hibmc_mm_fini(struct hibmc_drm_private *hibmc);
 int hibmc_dumb_create(struct drm_file *file, struct drm_device *dev,
 		      struct drm_mode_create_dumb *args);
+int hibmc_ddc_create(struct drm_device *drm_dev, struct hibmc_connector *connector);
 
 extern const struct drm_mode_config_funcs hibmc_mode_funcs;
 
