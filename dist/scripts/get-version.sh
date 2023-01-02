5e17d450c527d4ac14c1dc2bac66e5e9d3f08f90
Kairui Song <kasong@tencent.com>
2023-01-03 00:45:00 +0800

    dist: allow override KDIST for a one time build
    Upstream: no
    
    Make it possible to use:
    make KDIST=<value/none> dist-rpm/dist-srpm
    
    To override KDIST for a one time build.
    
    Also rename DISTRO= to DIST=, which is easier to memorize.
    
    Signed-off-by: Kairui Song <kasong@tencent.com>

--- dist/scripts/get-version.sh
+++ dist/scripts/get-version.sh
@@ -2,10 +2,22 @@
 # SPDX-License-Identifier: GPL-2.0
 #
 # Print out the tkernel version based on git commit and work tree.
-
+#
+# $1: git reference, tag or commit
+# $2: version type, 'nvr' or 'unamer', defaults to 'unamer'
+#
 # shellcheck source=./lib-version.sh
 . "$(dirname "$(realpath "$0")")/lib-version.sh"
 
-prepare_kernel_ver "$@"
-
-echo "$KERNEL_UNAMER"
+prepare_kernel_ver "$1"
+case $2 in
+	vr )
+		echo "$KERNEL_MAJVER"-"$KERNEL_RELVER"
+		;;
+	nvr )
+		echo "$KERNEL_NAME"-"$KERNEL_MAJVER"-"$KERNEL_RELVER"
+		;;
+	* | unamer )
+		echo "$KERNEL_UNAMER"
+		;;
+esac
